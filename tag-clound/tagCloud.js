c12c8065720d99a9378b6e938e16e726606ba5db
simkessy <simkessy@gmail.com>
2016-09-16 10:08:31 -0400

    Create tagCloud.js
    Create a list of all unique tags for existing items and return count

---
+++ tag-clound/tagCloud.js
@@ -0,0 +1,48 @@
+$(function() {
+	init();
+})
+
+var init = function() {
+	get()
+	.then(processTags);
+}
+
+var get = function() {
+	var d = $.Deferred();
+	var get = $().SPServices.SPGetListItemsJson({ 
+		listName: "+AskDCEP"
+	}) 
+
+	get.then(good, bad)
+
+	function good() {
+		d.resolve(this.data)
+	}
+
+	function bad() {
+		alert('fucked up')
+		d.reject('nope')
+	}
+
+	return d.promise();
+}
+
+var processTags = function(data) {
+	// do something with the data
+	var tags = _.chain(data)
+		.map(function(x) {return  x.Tags})
+		.flattenDeep()
+		.countBy()
+		.value();
+
+	var html = ''
+
+	_.forOwn(tags, function(value, key) {
+		var url = "/sites/Report.aspx?View={0D212EEF-0B1C-475F-A23A-8EED347505E2}&FilterField1=Tags&FilterValue1=" + key
+
+		html += "<a target='_blank' class='list-group-item' href='" + url + "'>" + key + "<span class='badge'>" +value + "</span></a>"
+	})
+
+	$('#container > ul').append(html)
+
+}
